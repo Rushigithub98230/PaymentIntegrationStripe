f2bb2c8e80f67fc4b4e51bd5ce39f7b3e90f044a
Rushikesh Chaudhary <109174203+Rushigithub98230@users.noreply.github.com>
2026-08-24 12:37:02 +0530

    feat: configure order persistence

---
+++ backend/PaymentIntegrationStripe.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaymentIntegrationStripe.Domain.Orders;
+
+namespace PaymentIntegrationStripe.Infrastructure.Persistence.Configurations;
+
+public sealed class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Subtotal).HasPrecision(19, 4).IsRequired();
+        builder.Property(x => x.Discount).HasPrecision(19, 4).IsRequired();
+        builder.Property(x => x.Tax).HasPrecision(19, 4).IsRequired();
+        builder.Property(x => x.Shipping).HasPrecision(19, 4).IsRequired();
+        builder.Property(x => x.Total).HasPrecision(19, 4).IsRequired();
+        builder.Property(x => x.Currency).HasMaxLength(3).IsRequired();
+        builder.Property(x => x.Status).HasConversion<string>().HasMaxLength(32).IsRequired();
+        builder.HasIndex(x => new { x.CustomerId, x.CreatedAtUtc });
+        builder.HasMany(x => x.Items).WithOne().HasForeignKey(x => x.OrderId).OnDelete(DeleteBehavior.Cascade);
+    }
+}
